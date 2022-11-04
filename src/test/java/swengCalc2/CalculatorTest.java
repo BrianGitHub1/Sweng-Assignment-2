519d24a25c67f37f4ca770caa974b4e4747cda9e
Brian B <bredicab@tcd.ie>
2022-11-04 17:52:10 +0000

    changed tests to float string and added new tests

--- src/test/java/swengCalc2/CalculatorTest.java
+++ src/test/java/swengCalc2/CalculatorTest.java
@@ -15,7 +15,7 @@ public class CalculatorTest {
 	public void testadd() {
 		
 		Calculator calc =new Calculator();
-		String expectedResult ="100";
+		String expectedResult ="100.0";
 		String in = "50+50";
 		
 		assertEquals(calc.equate(in), expectedResult);
@@ -26,7 +26,7 @@ public class CalculatorTest {
 	public void testminus() {
 		
 		Calculator calc =new Calculator();
-		String expectedResult ="10";
+		String expectedResult ="10.0";
 		String in = "100-90";
 		
 		assertEquals(calc.equate(in), expectedResult);
@@ -37,7 +37,7 @@ public class CalculatorTest {
 		public void testMul() {
 		
 		Calculator calc =new Calculator();
-		String expectedResult ="100";
+		String expectedResult ="100.0";
 		String in = "10*10";
 		
 		assertEquals(calc.equate(in), expectedResult);
@@ -48,7 +48,7 @@ public class CalculatorTest {
 		public void testDivide() {
 		
 		Calculator calc =new Calculator();
-		String expectedResult ="5";
+		String expectedResult ="5.0";
 		String in = "50/10";
 		
 		assertEquals(calc.equate(in), expectedResult);
@@ -59,7 +59,7 @@ public class CalculatorTest {
 	public void testNeg() {
 		
 		Calculator calc =new Calculator();
-		String expectedResult ="-100";
+		String expectedResult ="-100.0";
 		String in = "100-200";
 		
 		assertEquals(calc.equate(in), expectedResult);
@@ -71,7 +71,7 @@ public class CalculatorTest {
 		public void testInput1() {
 		
 		Calculator calc =new Calculator();
-		String expectedResult ="125";
+		String expectedResult ="125.0";
 		String in = "100+50/2";
 		
 		assertEquals(calc.equate(in), expectedResult);
@@ -81,22 +81,39 @@ public class CalculatorTest {
 		public void testInput2() {
 			
 			Calculator calc =new Calculator();
-			String expectedResult ="725";
-			String in = "100+50/2*25";
+			String expectedResult ="1400.0";
+			String in = "100+52*25";
 			
 			assertEquals(calc.equate(in), expectedResult);
 		
 	}
 	
 	@Test 
-		public void testInput3() {
+		public void testDecimal1() {
 			
 			Calculator calc =new Calculator();
-			String expectedResult ="-76396";
-			String in = "12435+34569-12345*10+50";
+			String expectedResult ="218.5";
+			String in = "118.0+100.5";
 			
 			assertEquals(calc.equate(in), expectedResult);
 		
 	}
+	
+	
+	
+	
+	@Test 
+		public void testDecimal2() {
+			
+			Calculator calc =new Calculator();
+			String expectedResult ="6.5";
+			String in = "3.25+3.25";
+			
+			assertEquals(calc.equate(in), expectedResult);
+		
+	}
+	
+	
+	
 	
 }
